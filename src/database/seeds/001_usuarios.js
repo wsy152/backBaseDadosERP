a7c07dedb0cbca8bcb41f5624a16f102c8afe561
Edvaldo Santana <edvaldosantana@Mac-mini-de-Edvaldo.local>
2021-02-17 09:18:46 -0300

    004 - Melhoria na conexão com banco de dados

--- src/database/seeds/001_usuarios.js
+++ src/database/seeds/001_usuarios.js
@@ -5,7 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('usuarios').insert([
-        { usu_name: 'Edvaldo Fernandes Santana'},
+        { usu_name: 'Edvaldo Fernandes Santana',usu_password:'123'},
+        { usu_name: 'Felipe Meireles',usu_password:'123'},
+        {usu_name: 'Carla Meireles',usu_password:'123'},
       ]);
     });
 };
